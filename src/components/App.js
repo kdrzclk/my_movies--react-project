d7f9b32d61b5fbb3a692d7549c48529e2bc7d5a8
kdrzclk <kadir.ozcelik16@gmail.com>
2022-03-29 03:17:37 +0300

    delete event kısmı dahil tamamlandı

---
+++ src/components/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import SearchBar from "./SearchBar.js";
+import MovieList from "./MovieList.js";
+
+class App extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        name: "The Flash",
+        rating: 8.3,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
+      },
+
+      {
+        id: 2,
+        name: "Interstellar",
+        rating: 6.8,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+      },
+
+      {
+        id: 3,
+        name: "Arrow",
+        rating: 7.9,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
+      },
+    ],
+  };
+
+  deleteMovie = (movie) => {
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+    this.setState({
+      movies: newMovieList,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">{/* <SearchBar /> */}</div>
+          </div>
+          <MovieList
+            movies={this.state.movies}
+            deleteMovieProp={this.deleteMovie}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>My Movies</h1>
+//     </div>
+//   );
+// };
+
+export default App;
