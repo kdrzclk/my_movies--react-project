8f18d9ae4d7814e3c2b89ad2ef872902bf6ac708
kdrzclk <kadir.ozcelik16@gmail.com>
2022-03-30 15:19:46 +0300

    axios api

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar.js";
 import MovieList from "./MovieList.js";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -8,25 +9,55 @@ class App extends React.Component {
     searchQuery: "",
   };
 
+  // fetch ile
+  // async componentDidMount() {
+  //   const baseURL = "http://localhost:3002/movies";
+  //   const response = await fetch(baseURL);
+  //   console.log(response);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   this.setState({ movies: data });
+  // }
+
   async componentDidMount() {
-    const baseURL = "http://localhost:3002/movies";
-    const response = await fetch(baseURL);
+    const response = await axios.get("http://localhost:3002/movies");
     console.log(response);
-    const data = await response.json();
-    console.log(data);
-    this.setState({ movies: data });
+    this.setState({ movies: response.data });
   }
 
-  deleteMovie = (movie) => {
-    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  // deleteMovie = (movie) => {
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
-    // this.setState({
-    //   movies: newMovieList,
-    // });  // array boş olsaydı herhangi bir bilgi olmasaydı daha mantıklıydı.
+  //   // this.setState({
+  //   //   movies: newMovieList,
+  //   // });  // array boş olsaydı herhangi bir bilgi olmasaydı daha mantıklıydı.
+
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   })); // listeyi güncellemek için bunu kullanmak daha mantıklı.
+  // };
+
+  // Fetch API
+  // deleteMovie = async (movie) => {
+  //   const baseURL = `http://localhost:3002/movies/${movie.id}`;
+  //   await fetch(baseURL, {
+  //     method: "DELETE",
+  //   });
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
+
+  //Axios API
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`);
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
     this.setState((state) => ({
       movies: newMovieList,
-    })); // listeyi güncellemek için bunu kullanmak daha mantıklı.
+    }));
   };
 
   searchMovie = (e) => {
@@ -44,7 +75,7 @@ class App extends React.Component {
     });
     return (
       <div>
-        <div className="container">
+        <div className="container" style={{ width: "75%" }}>
           <div className="row">
             <div className="col-lg-12">
               <SearchBar searchMovieProp={this.searchMovie} />
